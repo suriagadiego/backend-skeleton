40d11d90875a86994303a09c420ab9969c6b37ea
diegos <diego_suriaga@trendmicro.com>
2022-08-28 21:43:59 +0800

    cors test 4

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -63,10 +63,10 @@ ROOT_URLCONF = 'cfehome.urls'
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3030',
+    'http://localhost:3000',
 ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    'http://localhost:3030',
+    'http://localhost:3000',
 ]
 TEMPLATES = [
     {
