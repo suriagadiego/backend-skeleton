49496781dd131c14b6f239d6f822282e25cb9373
diegos <diego_suriaga@trendmicro.com>
2022-08-28 22:46:05 +0800

    ewan q na

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'cfehome.middleware.CorsMiddleware',
+    'cfehome.middleware.corsMiddleware'
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
